f3b4574ab722a30b46e7e313d347238cd75b8703
Adam Lamont <lamonta@gmail.com>
2015-09-29 08:28:06 -0400

    Initial Commit

---
+++ src/Quiz2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/**
+ *
+ * @author lamon
+ */
+public class Quiz2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Can use Wall1.txt, Wall2.txt, or Wall3.txt
+        City kw = new City("Wall1.txt");
+        Robot karel = new Robot(kw, 1, 1, Direction.WEST);
+        
+        //program solutions coded under here
+       
+    }
+    
+}
